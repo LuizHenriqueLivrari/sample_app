3671f480d6f9788c169f7bcbf353727d6e5a609f
mulletao@me.com <mulletao@me.com>
2013-04-19 10:43:58 -0300

    Finalizado Capitulo 9

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
 
 	before_filter :signed_in_user,	only: [ :edit, :update, :index ]
 	before_filter :correct_user,	only: [ :edit, :update ]
+	before_filter :admin_user,		only: [ :destroy ]
 
 	def show
 		@user = User.where( id: params[ :id ] ).first
@@ -39,6 +40,12 @@ class UsersController < ApplicationController
 		@users = User.paginate( page: params[ :page ])
 	end
 
+	def destroy
+		User.destroy( params[ :id ] )
+		flash[ :success ] = "User destroyed."
+		redirect_to users_url
+	end
+
 	private
 	
 	def signed_in_user
@@ -51,6 +58,10 @@ class UsersController < ApplicationController
 	def correct_user
 		@user = User.where( id: params[ :id ] ).first
 		redirect_to root_path unless current_user?( @user )
+	end
+
+	def admin_user
+		redirect_to root_path unless current_user.admin?
 	end
 
 end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -8,7 +8,7 @@ describe "User pages" do
 
 		let(:user) { FactoryGirl.create(:user) }
 
-		before(:each) do
+		before do
 			sign_in user
 			visit users_path
 		end
@@ -28,7 +28,28 @@ describe "User pages" do
 					page.should have_selector('li', text: user.name)
 				end
 			end
+
 		end
+
+		describe "delete links" do
+
+			it { should_not have_link('delete') }
+
+			describe "as an admin user" do
+				let(:admin) { FactoryGirl.create(:admin) }
+				before do
+					sign_in admin
+					visit users_path
+				end
+
+				it { should have_link('Delete', href: user_path(User.first)) }
+				it "should be able to delete another user" do
+					expect { click_link('Delete') }.to change(User, :count).by(-1)
+				end
+				it { should_not have_link('Delete', href: user_path(admin)) }
+			end
+		end
+
 	end
 
 	describe "profile page" do
