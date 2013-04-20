f11f899a4ae0ee3caf803536b06b8522d2cdbfb6
Luiz Henrique <mulletao@me.com>
2013-04-19 23:40:46 -0300

    Quase no fim da capitulo 11...

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,17 +1,19 @@
 class StaticPagesController < ApplicationController
-	
+
 	def home
-		@micropost = current_user.microposts.build if signed_in?
-		@feed_items = current_user.feed.paginate( page: params[ :page ] )
+		if signed_in?
+			@micropost = current_user.microposts.build
+			@feed_items = current_user.feed.paginate( page: params[ :page ] )
+		end
 	end
-	
+
 	def help
 	end
-	
+
 	def about
 	end
-	
+
 	def contact
 	end
-	
+
 end
